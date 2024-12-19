005d23ff4189f72dd96c34c504e4e52de3a92e55
patrick-schultz <pschultz@broadinstitute.org>
2024-12-19 09:37:13 -0500

    move mill modules into subdirectory

--- hail/hail/test/src/is/hail/expr/ir/TableIRSuite.scala
+++ hail/hail/test/src/is/hail/expr/ir/TableIRSuite.scala
@@ -672,10 +672,10 @@ class TableIRSuite extends HailSuite {
 
   // Catches a bug in the partitioner created by the importer.
   @Test def testTableJoinOfImport(): Unit = {
-    val mnr = MatrixNativeReader(fs, getTestResource("sample.vcf.mt"))
+    val mnr = MatrixNativeReader(fs, "hail/test/resources/sample.vcf.mt")
     val mt2 = MatrixRead(mnr.fullMatrixType, false, false, mnr)
     val t2 = MatrixRowsTable(mt2)
-    val mt = importVCF(ctx, getTestResource("sample.vcf"))
+    val mt = importVCF(ctx, "hail/test/resources/sample.vcf")
     var t: TableIR = MatrixRowsTable(mt)
     t = TableMapRows(
       t,
@@ -686,7 +686,7 @@ class TableIRSuite extends HailSuite {
   }
 
   @Test def testNativeReaderWithOverlappingPartitions(): Unit = {
-    val path = getTestResource("sample.vcf-20-partitions-with-overlap.mt/rows")
+    val path = "hail/test/resources/sample.vcf-20-partitions-with-overlap.mt/rows"
     // i1 overlaps the first two partitions
     val i1 = Interval(Row(Locus("20", 10200000)), Row(Locus("20", 10500000)), true, true)
 
